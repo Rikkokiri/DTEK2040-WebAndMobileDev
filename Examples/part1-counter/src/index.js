365f6c9b161054d987851adb9ae1fa3075c78963
Rikkokiri <pilvi.rajala@gmail.com>
2019-02-07 13:02:36 +0200

    Implementation of counter based on change of state.

--- Examples/part1-counter/src/index.js
+++ Examples/part1-counter/src/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: 1
+        }
+
+        /* The state of React components in the variable this.state should not be updated directly.
+         * Instead, you must always use function setState for this purpose.
+         * Calling the function updates the state and causes the component to be re-rendered
+         * (assuming this has not been prevented, as we will discuss later). */
+        setInterval(() => {
+            this.setState({counter: this.state.counter + 1 })
+        }, 1000)
+    }
+
+    render() {
+        return (
+            <div>{this.state.counter}</div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />, document.getElementById('root')
+)
+
+/*
 const App = (props) => {
     const {counter} = props
     
@@ -27,3 +55,4 @@ setInterval(() => {
     rendering()
     counter.value += 1;
 }, 1000)
+*/
