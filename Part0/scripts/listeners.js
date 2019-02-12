55249b42e421c50d2d87173a4352cdc16b2405f2
Rikkokiri <pilvi.rajala@gmail.com>
2019-02-12 11:06:24 +0200

    Product image changes corresponding to form selections.

--- Part0/scripts/listeners.js
+++ Part0/scripts/listeners.js
@@ -1,5 +1,43 @@
+let color = 'black';
+let bundle = 'nobundle';
+
 function addFormListeners() {
+
     $('#colorselector label').on('click', function(){
-        
+        switch( $("input", this).val() ) {
+            case 'black':
+                color = 'black';
+                break;          
+            case 'green':
+                color = 'green';
+                break;
+            case 'blue':
+                color = 'blue';
+                break;  
+        }
+
+        setProductImage()
     });
+
+    $('#bundleselector label').on('click', function(){
+        switch( $("input", this).val() ) {
+            case 'nobundle':
+                bundle = 'nobundle';
+                break;          
+            case 'travelbundle':
+                bundle = 'travelbundle';
+                break;
+            case 'photobundle':
+                bundle = 'photobundle';
+                break;  
+        }
+
+        setProductImage()
+    });
+   
+}
+
+function setProductImage() {
+    path = "img/prvke/"+bundle+"-"+color+".jpg"
+    $("#main-product-photo").attr('src', path);
 }
