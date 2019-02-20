9f3e8ccee2417a4eb584bb08764811841b53ff64
Rikkokiri <pilvi.rajala@gmail.com>
2019-02-20 14:34:53 +0200

    Styled the feedback app

--- Part1/feedback-app/src/index.js
+++ Part1/feedback-app/src/index.js
@@ -2,24 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const CustomButton = ({text, clickHandler}) => (
+const CustomButton = ({ text, clickHandler }) => (
     <button onClick={clickHandler} className="CustomButton">
         {text}
     </button>
 )
 
-const Statistics = ({data}) => {
+const Statistics = ({ data }) => {
     // Calculate percentage of positives
     const positive = (positiveCount, totalCount) => {
-        return Math.round((positiveCount / totalCount)*1000 ) / 10;
+        return Math.round((positiveCount / totalCount) * 1000) / 10;
     }
 
     // Calculate average
     const average = (valueSum, totalCount) => {
-        return Math.round( (valueSum / totalCount) * 100 ) / 100;
+        return Math.round((valueSum / totalCount) * 100) / 100;
     }
 
-    if(data.voteCount === 0) {
+    if (data.voteCount === 0) {
         return (
             <div>No statistics available</div>
         )
@@ -28,18 +28,18 @@ const Statistics = ({data}) => {
     return (
         <table>
             <tbody>
-                <Statistic name="Good" value={data.good}/>
-                <Statistic name="Neutral" value={data.neutral}/>
-                <Statistic name="Bad" value={data.bad}/>
-                <Statistic name="Average" value={average(data.sum, data.voteCount)}/>
+                <Statistic name="Good" value={data.good} />
+                <Statistic name="Neutral" value={data.neutral} />
+                <Statistic name="Bad" value={data.bad} />
+                <Statistic name="Average" value={average(data.sum, data.voteCount)} />
                 <Statistic name="Positive" value={positive(data.good, data.voteCount).toString() + " %"} />
             </tbody>
         </table>
     )
 }
 
-const Statistic = ({name, value}) => (
-        <tr><td>{name}</td><td>{value}</td></tr>
+const Statistic = ({ name, value }) => (
+    <tr><td>{name}</td><td>{value}</td></tr>
 )
 
 class App extends React.Component {
@@ -76,7 +76,7 @@ class App extends React.Component {
             voteCount: this.state.voteCount + 1
         })
     }
-    
+
     /*
     voteHandler = (name, value) => {
         this.setState({
@@ -88,14 +88,14 @@ class App extends React.Component {
     render() {
         return (
             <div className="AppContainer">
-                <h2>Give Feedback</h2>
-                <CustomButton text = "Good" clickHandler={this.clickGood} />
-                <CustomButton text = "Neutral" clickHandler={this.clickNeutral} />
-                <CustomButton text = "Bad" clickHandler={this.clickBad} />
-                
+                <h1>Give Feedback</h1>
+                <CustomButton text="Good" clickHandler={this.clickGood} />
+                <CustomButton text="Neutral" clickHandler={this.clickNeutral} />
+                <CustomButton text="Bad" clickHandler={this.clickBad} />
+
                 <div className="StatisticsContainer">
                     <h2>Statistics</h2>
-                    <Statistics data={this.state}/>
+                    <Statistics data={this.state} />
                 </div>
             </div>
         )
