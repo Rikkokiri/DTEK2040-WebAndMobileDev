88f4a269c41e1378454b29b8978e6926367cfc9d
Rikkokiri <pilvi.rajala@gmail.com>
2019-03-15 13:22:13 +0200

    Part 3 tutorial finished

--- Examples/part3-helloworld/index.js
+++ Examples/part3-helloworld/index.js
@@ -1,9 +1,9 @@
 // const http = require('http')
 
 /*
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(notes))
+const app = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(notes))
 })
 
 const port = 3001
@@ -16,6 +16,8 @@ console.log(`Server running on port ${port}`)
 const express = require('express')
 const app = express() /* By calling the function express, we assign an object
                          representing an Express application to the variable app */
+
+const Note = require('./models/note')
 
 // --------------- Middleware -----------------
 /*
@@ -73,32 +75,56 @@ let notes = [
   }
 ]
 
+// Format data retrieved from the database
+const formatNote = (note) => {
+  return {
+    content: note.content,
+    date: note.date,
+    important: note.important,
+    id: note._id
+  }
+}
+
 // -------------- Routes --------------
 
-app.get('/api/', (req, res) => {
-  res.send('<h1>Note app backend</h1>')
+app.get('/api/', (request, response) => {
+  response.send('<h1>Note app backend</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (request, response) => {
+  Note
+    .find({}, { __v: 0 })
+    .then(notes.map(formatNote))
+    .then(formattedNotes => {
+      response.json(formattedNotes)
+    })
 })
 
-app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
+app.get('/api/notes/:id', (request, response) => {
+  Note
+    .findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(formatNote(note))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: "malformatted id" })
+    })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res
-  res.status(204).end()
+app.delete('/api/notes/:id', (request, response) => {
+  Note
+    .findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: "malformatted id" })
+    })
 })
 
 const generateId = () => {
@@ -106,26 +132,49 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post('/api/notes', (req, res) => {
-  const body = req.body
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
   if (body.content === undefined) {
     /* Note that the command return is important here. Otherwise the execution would continue until the end of the method
     and an erroneous note would be saved in the database! */
-    return res.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: 'content missing' })
   }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
+
+  note
+    .save()
+    .then(formatNote)
+    .then(savedAndFormattedNote => {
+      response.json(savedAndFormattedNote)
+    })
+})
+
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
 
   const note = {
     content: body.content,
-    important: body.important || false,
-    data: new Date(), // Timestamp should be created on the server side because client's computer cannot be trusted
-    id: generateId()
+    important: body.important
   }
 
-  notes = notes.concat(note)
-
-  res.json(note)
+  Note
+    .findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(formatNote)
+    .then(updatedAndFormattedNote => {
+      response.json(updatedAndFormattedNote)
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
